a7f547ab605ab2f0151ab564be2512cddacee5d6
Craymotherlol <Craymotherlol@gmail.com>
2023-08-21 14:31:15 +0200

    Server 2008 page

--- Javascript/Server2008.js
+++ Javascript/Server2008.js
@@ -2,7 +2,7 @@
 let OSIN;
 async function fetchOSInfo() {
   try {
-    const response = await fetch(' Windows8.json');
+    const response = await fetch('https://realcraymotherlol.github.io/JSON/WindowsVista.json');
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
