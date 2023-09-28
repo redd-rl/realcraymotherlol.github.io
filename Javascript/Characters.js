ff7064cca5b886f48b75d9c124d0f2c0204e6c9d
Craymotherlol <Craymotherlol@gmail.com>
2023-09-28 10:34:40 +0200

    JSON test

--- Javascript/Characters.js
+++ Javascript/Characters.js
@@ -3,8 +3,6 @@ const pagination = document.getElementById("pagination");
 const prevButton = document.getElementById("prevbtn");
 const nextButton = document.getElementById("nextbtn");
 const pageLinks = pagination.querySelectorAll("a");
-
-const itemsPerPage = 21;
 let currentPage = 1;
 
 function openNav() {
@@ -18,15 +16,35 @@ function closeNav() {
 }
 
 function displayItemsForPage(pageNumber) {
-    const artboxes = document.querySelectorAll(".chrbox");
-    artboxes.forEach((box, index) => {
-        if (index >= (pageNumber - 1) * itemsPerPage && index < pageNumber * itemsPerPage) {
-            box.style.display = "block";
-        } else {
-            box.style.display = "none";
-        }
-    });
+    const artboxes = document.querySelectorAll(".boximg");
+    const itemsPerPage = 21;
+
+    fetch('https://realcraymotherlol.github.io/JSON/paginationbowtop.json')
+        .then(response => response.json())
+        .then(data => {
+            const currentPageData = data.ID.Currentpage[pageNumber.toString()];
+
+            artboxes.forEach((box, index) => {
+                if (index < itemsPerPage) {
+                    const imgboxKey = "imgbox-" + (index + 1);
+                    if (currentPageData.hasOwnProperty(imgboxKey)) {
+                        const backgroundImage = currentPageData[imgboxKey]["background-image"];
+                        box.style.backgroundImage = `url(${backgroundImage})`;
+                    } else {
+                        box.style.backgroundImage = "none";
+                    }
+                } else {
+                    box.style.backgroundImage = "none";
+                }
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching JSON data:", error);
+        });
 }
+
+displayItemsForPage(1);
+
 
 displayItemsForPage(currentPage);
 
