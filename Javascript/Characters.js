cfe105f0fba3e25fec89080ce9ce8daa080ffa2e
craymotherlol <craymotherlol@gmail.com>
2023-10-03 23:49:30 +0200

    update path and change css

--- Javascript/Characters.js
+++ Javascript/Characters.js
@@ -6,20 +6,6 @@ const pageLinks = pagination.querySelectorAll("a");
 var mobile = window.matchMedia("(max-width: 600px)")
 let currentPage = 1;
 
-function openNav() {
-    if (mobile.matches) {
-        document.getElementById("mySidepanel").style.width = "100%";
-    } else {
-        document.getElementById("mySidepanel").style.width = "32vw";
-    }
-    document.getElementById("mySidepanel").style.borderLeft = "5px solid rgb(175, 118, 11)";
-}
-
-function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-    document.getElementById("mySidepanel").style.borderLeft = "0px solid rgb(175, 118, 11)";
-}
-
 function navigateToPage(pageNumber) {
     currentPage = pageNumber;
     displayItemsForPage(currentPage);
